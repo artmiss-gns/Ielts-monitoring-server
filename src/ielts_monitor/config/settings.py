8d37a3c45cd0bedac67f71b6ac355b6976872a88
Artmiss <artmiss.gns@gmail.com>
2025-09-30 23:44:55 +0330

    test github actions with temporary test server

--- src/ielts_monitor/config/settings.py
+++ src/ielts_monitor/config/settings.py
@@ -10,7 +10,8 @@ class ScraperConfig(BaseModel):
     """Configuration for the scraper."""
     
     # Base URL for the IELTS appointment website
-    base_url: str = "https://irsafam.org/ielts/timetable"
+    # base_url: str = "https://irsafam.org/ielts/timetable"
+    base_url: str = "77.91.84.205:8000" # to test github-actions
     
     # User agent to mimic a real browser
     user_agent: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
